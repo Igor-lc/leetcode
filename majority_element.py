83caffe6b63340db4e942ebe12714436a6aa9463
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-11-30 14:00:14 +0200

    Add files via upload

--- majority_element.py
+++ majority_element.py
@@ -1,5 +1,12 @@
-from collections import Counter, defaultdict
+'''Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+Input: nums = [3,2,3]
+Output: 3
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+Follow-up: Could you solve the problem in linear time and in O(1) space?'''
 
+from collections import Counter, defaultdict
 
 # ver_4 space complexity O(1)
 class Solution4:
